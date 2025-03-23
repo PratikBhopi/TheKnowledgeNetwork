6fc1c88b5b710551dcbc911c30f0acb84558e88f
Pratik <pkbhopi132@gmail.com>
2025-03-23 10:24:15 +0530

    project structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+]);
 
 const App = () => {
-  
+  return (
+    <RouterProvider router={router} />
+  )
 };
 
 export default App;
